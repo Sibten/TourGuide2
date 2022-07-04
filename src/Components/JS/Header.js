8edc8fd65c7d8d8180908288417f7d11891866c4
Sibten <shekhultra@gmail.com>
2022-07-04 11:14:13 +0530

    Developer Mode Deactive

--- src/Components/JS/Header.js
+++ src/Components/JS/Header.js
@@ -17,7 +17,7 @@ export default function Header() {
             <Nav.Link className="NavItem"> <Link to="/" className="Link"> <FaHome/> &nbsp;&nbsp;Home </Link></Nav.Link>
             <Nav.Link  className="NavItem"> <Link to="/Places" className="Link"> <FaLocationArrow/> &nbsp;&nbsp;Places </Link></Nav.Link>
             <Nav.Link  className="NavItem"><Link to="/Train" className="Link"> <FaTrain/> &nbsp;&nbsp; Trains </Link></Nav.Link>
-            <Nav.Link  className="NavItem"><Link to="/Devloper" className="Link"> <FaCode/> &nbsp;&nbsp; Devloper </Link></Nav.Link>
+            {/* <Nav.Link  className="NavItem"><Link to="/Devloper" className="Link"> <FaCode/> &nbsp;&nbsp; Devloper </Link></Nav.Link> */}
         </Nav>
         </Navbar.Collapse>
         </Container>
